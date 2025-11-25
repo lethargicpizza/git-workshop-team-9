f8be19c2053a6657016416479587ecaaad3c5eb8
Per Nordenbrink <permobil80@outlook.com>
2025-11-25 14:37:17 +0100

    feat(student): add email field

---
+++ Student.java
@@ -0,0 +1,9 @@
+[200~public class Student {
+    private String name;
+    private String email;  // Person A lägger till email
+
+    public Student(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
